356db7d26d5849f2692c31d6c5f381a7c35d0d41
ALBERT\Albert <albert.figuerola.gmz@gmail.com>
2024-01-31 16:42:29 +0100

    Added the entity user, userController, userRepository and refactor de FilmService and FilmServiceImpl.

--- src/main/java/com/example/demo/entity/Film.java
+++ src/main/java/com/example/demo/entity/Film.java
@@ -1,28 +1,53 @@
 package com.example.demo.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+import java.util.UUID;
 
 @Entity
 public class Film {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
 
     private String description;
 
+    @ManyToMany
+    @JoinTable(
+            name = "rating_films",
+            joinColumns = @JoinColumn(name = "film_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id")
+    )
+    Set<Film> filmScore;
+
+    private LocalDateTime created_at;
+
+    private LocalDateTime updated_at;
+
     public Film() {
+        this.created_at = LocalDateTime.now();
+        this.updated_at = updated_at;
     }
 
     public Film(Long id, String name, String description) {
         this.id = id;
         this.name = name;
         this.description = description;
+        this.created_at = created_at;
+        this.updated_at = updated_at;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getName() {
@@ -39,5 +64,29 @@ public class Film {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+    /*
+    public Set<Film> getFilmScore() {
+        return filmScore;
+    }
+
+    public void setFilmScore(Set<Film> filmScore) {
+        this.filmScore = filmScore;
+    }
+    */
+    public LocalDateTime getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(LocalDateTime created_at) {
+        this.created_at = created_at;
+    }
+
+    public LocalDateTime getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(LocalDateTime updated_at) {
+        this.updated_at = updated_at;
     }
 }
---
+++ src/main/java/com/example/demo/service/impl/FilmServiceImpl.java
@@ -0,0 +1,33 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.entity.Film;
+import com.example.demo.repository.FilmRepository;
+import com.example.demo.service.FilmService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class FilmServiceImpl implements FilmService {
+    @Autowired
+    private FilmRepository filmRepository;
+
+    @Override
+    public void addFilm(Film film) {
+        filmRepository.save(film);
+    }
+
+    @Override
+    public List<Film> getAllFilms() {
+        return filmRepository.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        filmRepository.deleteById(id);
+    }
+
+}
